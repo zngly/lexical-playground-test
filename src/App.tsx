8fefe9f27c7c3c67f9eb60cadbafdd2bc5572a67
ditto-vlad <vlad@zngly.com>
2024-05-23 10:16:36 +0100

    init playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import LexicalPlayground from "@zngly/lexical-playground";
+
 import "./App.css";
 
 const App = () => {
 	return (
 		<div className="content">
-			<h1>Rsbuild with React</h1>
-			<p>Start building amazing things with Rsbuild.</p>
+			<LexicalPlayground />
 		</div>
 	);
 };
